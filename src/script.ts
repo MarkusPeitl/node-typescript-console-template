2ac206ae05783827b3eb29f828f22814c3b21895
Markus Peitl <office@markuspeitl.com>
2021-08-20 18:40:33 +0200

    Added sample code into script, Added prettier and eslint

--- src/script.ts
+++ src/script.ts
@@ -1,2 +1,35 @@
-import argparse from "argparse";
-console.log("Script executed")
+import { ArgumentParser } from 'argparse';
+import fs from 'fs';
+
+console.log('Script executed');
+
+const parser = new ArgumentParser({
+	description: 'A sample console application template'
+});
+
+parser.add_argument('source', { help: 'Source path to consume' });
+parser.add_argument('target', { help: 'Target path to write to' });
+
+const args: any = parser.parse_args();
+
+function readDocument(targetFilePath: string): string | null {
+	console.log('Reading document from: ' + targetFilePath);
+	if (fs.existsSync(targetFilePath)) {
+		return fs.readFileSync(targetFilePath) as unknown as string;
+	}
+	return null;
+}
+function writeDocument(contents: string, targetFilePath: string): void {
+	console.log('Writing: ' + contents + '\nto: ' + targetFilePath);
+	fs.writeFileSync(targetFilePath, contents);
+}
+
+const documentContents: string | null = readDocument(args.source);
+
+if (documentContents) {
+    const newDocumentContents: string = documentContents + '\n Appended data for document output';
+
+    writeDocument(newDocumentContents, args.target);
+}
+
+console.log('Script finished -> exiting');
